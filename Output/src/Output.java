038aa46718eb2143ebc0d5d82c23f1aef2799f42
mohaneduestc <43443637+mohaneduestc@users.noreply.github.com>
2019-03-09 11:07:39 +0200

    Add files via upload
    To display the difference between x++ and ++x

---
+++ Output/src/Output.java
@@ -0,0 +1,27 @@
+
+public class Output {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Output o = new Output();
+		o.go();
+	}
+
+	void go() {
+		int y = 7;
+		for(int x = 1; x < 8; x++) {
+			y++;
+			if (x > 4) {
+				System.out.print(++y + " ");
+			}
+			if (y > 14) {
+				System.out.println(" x = " + x);
+				break;
+			}
+		}
+	}
+}
